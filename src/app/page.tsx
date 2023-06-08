c8adca71998b3a98f9777d1b8c60c160e4848bf9
Eric Soares Silva <ericsoares51@hotmail.com>
2023-06-08 20:07:16 -0300

    chore: otimized input component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
+"use client";
+import CardComponent from "@/components/Card";
+import CardList from "@/components/CardList";
+import Input from "@/components/Input";
+import Select from "@/components/Select";
+import { Search } from "lucide-react";
 import Image from "next/image";
+import { useForm } from "react-hook-form";
 
 export default function Home() {
+  const { register } = useForm<{ name: string }>();
+
   return (
     <main>
-      <h1>Hello</h1>
+      <Input
+        placeholder="Pesquisar ..."
+        name="search"
+        icon={<Search />}
+        register={register}
+      />
     </main>
   );
 }
---
+++ src/components/Input/index.tsx
@@ -0,0 +1,31 @@
+import { HTMLProps } from "react";
+import { UseFormRegister } from "react-hook-form";
+
+interface InputProps extends HTMLProps<HTMLInputElement> {
+  icon?: React.ReactNode;
+  placeholder: string;
+  name: string;
+  register: UseFormRegister<any>;
+  background?: string;
+}
+
+export default function Input({
+  icon,
+  register,
+  name,
+  background,
+  ...rest
+}: InputProps) {
+  return (
+    <div
+      className={`flex flex-1 bg-gray-200 border-2 border-gray-300 rounded-md p-1.5 transition-all focus-within:border-blue-300`}
+    >
+      <input
+        className="bg-transparent focus:outline-none grow"
+        {...rest}
+        {...register(name)}
+      />
+      {icon}
+    </div>
+  );
+}
