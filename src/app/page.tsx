d4b3afe9a532455bff150db1a152a594b0db3947
Eric Soares Silva <ericsoares51@hotmail.com>
2023-06-08 21:02:34 -0300

    feat: Select component updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import CardList from "@/components/CardList";
-import Select from "@/components/Select";
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="bg-slate-300 grid place-items-center w-full h-screen overflow-hidden">
-      <div className="w-4/5 h-4/6 bg-white overflow-hidden">
-        <CardList cards={[]} />
-      </div>
+    <main>
+      <h1>Home</h1>
     </main>
   );
 }
