b278fed8a3baee53d15afdbcbe707a72cd1fb3d1
Dan Souza <darleyble3@gmail.com>
2023-06-13 15:18:41 -0300

    Aplicando Tailwind e removendo .css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import Login from '../components/Login';
+import UploadFile from '../components/UploadFile';
 
 function HomePage() {
   return (
-      <Login />
+      <UploadFile />
   );
 }
 
--- src/components/UploadFile/index.tsx
+++ src/components/UploadFile/index.tsx
@@ -52,31 +52,31 @@ export default function FileUpload() {
     <div className="w-[100vw] h-[100vh] bg-neutral-800 flex items-center justify-center">
       <div className="rounded-lg w-[30vw] h-[40vh] bg-white overflow-hidden flex justify-center items-center p-8">
         <div className="bg-white w-[90vw] m-auto p-1">
-          <div className="m-3 w-[25vw] h-[20vh] border-dashed border-4 border-[#283A73] hover:bg-[#283A73] hover:border-[#fff]">
+          <div className="flex items-center justify-center m-3 w-[25vw] h-[20vh] border-dashed border-4 border-[#283A73] hover:bg-[#283A73] hover:border-[#fff]">
             <input
               ref={fileInputRef}
-              className="absolute m-0 p-0 outline-0 opacity-0 cursor-pointer"
+              className="absolute p-0 m-0 w-[25%] h-[20%] opacity-0 cursor-pointer"
               type="file"
               onChange={handleFileChange}
               accept="application/pdf"
             />
-            <div className="">
-              <h3 className="flex items-center justify-center font-thin uppercase text-[#4561BD]">{dropText}</h3>
+            <div className="flex items-center justify-center">
+                <h3 className="uppercase text-[#4561BD]">{dropText}</h3>
             </div>
           </div>
-          <div className={styles.fileUploadContent}>
-            <div>
+          <div className="flex items-center justify-center whitespace-normal">
+            <div className="text-white font-bold">
               <button
                 type="button"
                 onClick={removeUpload}
-                className={styles.removePdfButton}
+                className="w-28 p-2 m-2 cursor-pointer rounded-md uppercase bg-[#283A73] hover:bg-[#4561BD]"
               >
                 Remover
               </button>
               <button
                 type="button"
                 onClick={handleFileSubmit}
-                className={styles.submitPdfButton}
+                className="w-28 p-2 m-2 cursor-pointer rounded-md uppercase bg-[#283A73] hover:bg-[#4561BD]"
               >
                 Enviar
               </button>
